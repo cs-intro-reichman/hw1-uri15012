89f073d9bba6b1f607f4c34948e75a2359c7f782
uri15012 <uri.herszenhaut01@post.runi.ac.il>
2024-11-09 21:26:07 +0200

    q1

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 
 		double amountPerPerson =  Math.ceil(totalBill / 3.0);
 
-		System.out.println("Dear" + name3 + "," + name2 + ", and " + name1 + ":" + "pay" + amountPerPerson + "shekels each");
+		System.out.println ( " Dear " + name3 + " , " + name2 + ", and " + name1 + " : " + " pay " + amountPerPerson + " shekels each ");
 
 	     
 	}
